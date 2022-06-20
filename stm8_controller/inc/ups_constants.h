01d543cd780757188b4d3b753b58201bd2663869
Tony Kuker <akuker@gmail.com>
2022-06-20 12:45:13 -0500

    cleanup

--- stm8_controller/inc/ups_constants.h
+++ stm8_controller/inc/ups_constants.h
@@ -12,11 +12,6 @@
 #pragma once
 #include <stdint.h>
 
-#define ERROR_CRC 0xFF
-#define ERROR_SIZE 0xFE
-#define ERROR_UNINITIALIZED 0xFD
-#define ERROR_SUCCESS 0
-
 typedef enum
 {
     STATE_DISABLED = (uint8_t)0,
@@ -28,15 +23,3 @@ typedef enum
     STATE_POWER_LOSS_SHUTDOWN_DELAY = (uint8_t)6,
     STATE_COMMANDED_POWER_CYCLE_DELAY = (uint8_t)7,
 } ups_state_type;
-
-#define VAL_MOSFET i2c_register_values[MOSFET]
-#define VAL_VIN_HIGH i2c_register_values[VIN_HIGH]
-#define VAL_VUPS_HIGH i2c_register_values[VUPS_HIGH]
-#define VAL_ON_THRESH i2c_register_values[ON_THRESH]
-#define VAL_OFF_THRESH i2c_register_values[OFF_THRESH]
-#define VAL_COUNTDOWN i2c_register_values[COUNTDOWN]
-#define VAL_STATE i2c_register_values[STATE]
-#define VAL_CYCLE_DELAY i2c_register_values[CYCLE_DELAY]
-#define VAL_FAIL_SHUTDOWN_DELAY i2c_register_values[FAIL_SHUTDOWN_DELAY]
-#define VAL_RUN_COUNTER i2c_register_values[RUN_COUNTER]
-#define VAL_POWERUP_THRESH i2c_register_values[POWERUP_THRESH]
