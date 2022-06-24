5fa59cc9941adc2ed9ccdebfaef0a5e6f10a5025
Tony Kuker <akuker@gmail.com>
2022-06-23 21:32:28 -0500

    Add gpio handler and device id

---
+++ stm8_controller/inc/gpio_handler.h
@@ -0,0 +1,16 @@
+//---------------------------------------------------------------------------
+//
+//	Raspberry Pi SuperUPS
+//
+//	Copyright (C) 2020 Scott Baker
+//	Copyright (C) 2022 akuker
+//
+//	[ GPIO Handler ]
+//
+//---------------------------------------------------------------------------
+
+#pragma once
+
+void gpios_init();
+void gpios_step();
+
